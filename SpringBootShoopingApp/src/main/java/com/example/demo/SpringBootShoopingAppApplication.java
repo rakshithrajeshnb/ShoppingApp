dec964aebe76d2fecc0e762c478957cc653cb3e4
Rakshith <rakshithrajesh55@gmail.com>
2025-01-22 11:07:41 +0530

    First Commit

---
+++ SpringBootShoopingApp/src/main/java/com/example/demo/SpringBootShoopingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShoopingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootShoopingAppApplication.class, args);
+	}
+
+}
