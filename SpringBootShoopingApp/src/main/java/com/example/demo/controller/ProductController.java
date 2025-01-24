1e0d6456d2417475bc0155e31677834fe2a7a0c9
Rakshith <rakshithrajesh55@gmail.com>
2025-01-24 11:20:28 +0530

    Logger

--- SpringBootShoopingApp/src/main/java/com/example/demo/controller/ProductController.java
+++ SpringBootShoopingApp/src/main/java/com/example/demo/controller/ProductController.java
@@ -1,10 +1,11 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Product;
@@ -13,6 +14,7 @@ import com.example.demo.service.ProductService;
 @RestController
 @RequestMapping("/products") //http://localhost:8081/products
 public class ProductController {
+	private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
 	@Autowired
 	ProductService service;
 	
@@ -20,7 +22,12 @@ public class ProductController {
 	@PostMapping("/save")  //http://localhost:8080/products/save
 	public String saveProduct(@RequestBody Product product)
 	{
+		logger.info("Received request to save product: {}", product);
+	    String response = service.addProduct(product);
+	    logger.info("Product saved successfully with response: {}", response);
 		return service.addProduct(product);
 	}
 	
 }
+
+
